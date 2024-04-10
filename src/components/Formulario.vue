0ffee03e2cddac7819cc3978b8c782f3136cdd33
Yuri Cruz <yuricruzf@gmail.com>
2024-04-09 23:19:52 -0300

    Using v-model to get the task

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,14 +1,27 @@
 <template lang="">
   <div class="box is-white">
     <div class="column">
-      <TemporiZador  />
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+      <!-- Linkando propriedades do input -->
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar"
+          v-model="desricaoTask"
+        />
+        </div>
+      <TemporiZador @temporizadorFinalizado="finalizarTarefa"/>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import Cronometro from './Cronometro.vue'
+//Components
 import TemporiZador from './Temporizador.vue'
 
 export default defineComponent({
@@ -16,6 +29,18 @@ export default defineComponent({
   components: {
     TemporiZador
   },
+  data() {
+    return {
+      desricaoTask: ''
+    }
+  },
+  methods: {
+    finalizarTarefa(tempoDecorridoRecebido: number): void {
+      console.log(tempoDecorridoRecebido);
+      console.log(this.desricaoTask);
+      this.desricaoTask = ''
+    }
+  }
 });
 </script>
 <style scoped></style>
