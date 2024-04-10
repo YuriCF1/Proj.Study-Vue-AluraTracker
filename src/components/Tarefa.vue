7ad1485f3f933bdcf9fbf1b4bbf077b1072790ae
Yuri Cruz <yuricruzf@gmail.com>
2024-04-10 16:56:18 -0300

    Adding props

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -2,16 +2,18 @@
     <div class="box has-text-weight-bold">
         <div class="columns">
             <div class="column is-7">
-                Descrição da Tarefa
+                {{tarefa.descricaoInterface}}
             </div>
             <div class="column">
-                <Cronometro tempoEmSegundos="15"/>
+                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundosInterface"/>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
+
+import ITarefa from '../Interfaces/ITarefa.ts'
 
 import Cronometro from './Cronometro.vue';
 
@@ -19,8 +21,11 @@ export default defineComponent({
     name: 'TareFa',
     components: {
         Cronometro
+    },
+    props: {
+        tarefa: Object as PropType<ITarefa>,
+        required: Boolean as true
     }
 })
-</script >
-<style scoped>
-</style>
+</script>
+<style scoped></style>
