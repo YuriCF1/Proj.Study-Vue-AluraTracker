b1edc7120175bd0b63709eb79f5009c0319f5d51
Yuri Cruz <yuricruzf@gmail.com>
2024-04-10 21:34:14 -0300

    Adding box component and props of task

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,31 +1,44 @@
 <template lang="">
-    <div class="box has-text-weight-bold">
+    <Box>
         <div class="columns">
             <div class="column is-7">
-                {{tarefa.descricaoInterface}}
+                {{tarefa.descricaoInterface || 'Tarefa sem descrição'}}
             </div>
             <div class="column">
                 <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundosInterface"/>
             </div>
         </div>
-    </div>
+    </Box>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 
-import ITarefa from '../Interfaces/ITarefa.ts'
+import ITarefa from '../Interfaces/ITarefa'
 
 import Cronometro from './Cronometro.vue';
+import Box from './Box.vue'
 
 export default defineComponent({
     name: 'TareFa',
     components: {
-        Cronometro
+        Cronometro, Box
     },
     props: {
-        tarefa: Object as PropType<ITarefa>,
-        required: Boolean as true
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        },
+        isRequired: {
+            type: Boolean,
+            default: true
+        }
     }
 })
 </script>
-<style scoped></style>
+<style scoped>
+/* .box {
+    margin: 2rem auto;
+    color: #fff;
+    font-size: 2rem;
+} */
+</style>
