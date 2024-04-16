73eb14494002d08cee839a718e129729c0b99547
Yuri Cruz <yuricruzf@gmail.com>
2024-04-16 20:51:50 -0300

    new path to project view

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,11 +1,19 @@
 import Tarefas from "@/Views/Tarefas.vue";
+import Projetos from "@/Views/Projetos.vue"
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
-    component: Tarefas
-}];
+const rotas: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tarefas',
+        component: Tarefas
+    },
+    {
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
+    }
+];
 
 const roteador = createRouter({
     history: createWebHashHistory(),
