b9b0a332d6efb66a7368432e1a05f7961b506fd3
Yuri Cruz <yuricruzf@gmail.com>
2024-04-16 20:51:08 -0300

    Adding new view

---
+++ src/Views/Projetos.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <h1>Projetos</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProjetosComponent'
+
+})
+</script>
+<style lang="">
+
+</style>
