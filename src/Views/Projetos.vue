60932306c084f69f8587fb58009993bcda74005b
Yuri Cruz <yuricruzf@gmail.com>
2024-04-17 23:57:50 -0300

    Interface and logic to add project

--- src/Views/Projetos.vue
+++ src/Views/Projetos.vue
@@ -1,14 +1,64 @@
 <template lang="">
-    <h1>Projetos</h1>
+    <section class="projetos">
+        <h1 class="title">
+            Projetos
+        </h1>
+        <form @submit.prevent="salvarProjeto">
+            <div class="field">
+                <label class="label" for="nomeDoProjeto" >
+                    Nome do projeto
+                </label>
+                <input class="input" type="text" name="" id="nomeDoProjeto" v-model="nomeDoProjeto"/>
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+                <tbody>
+                    <tr v-for="(projeto, index) in listaProjetos" :key="index">
+                        <td>{{projeto.id}}</td>
+                        <td>{{projeto.nome}}</td>
+                    </tr>
+                </tbody>
+        </table>
+    </section>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
+
+import IProjeto from '../Interfaces/IProjeto.ts'
 
 export default defineComponent({
-    name: 'ProjetosComponent'
+    name: 'ProjetosComponent',
+    data() {
+        return {
+            nomeDoProjeto: '',
+            listaProjetos: [],
+        }
+    },
+    methods: {
+        salvarProjeto() {
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: this.nomeDoProjeto
+            }
 
+            this.listaProjetos.push(projeto)
+        }
+    }
 })
 </script>
-<style lang="">
-
+<style scoped>
+.projetos {
+    padding: 1.25rem
+}
 </style>
