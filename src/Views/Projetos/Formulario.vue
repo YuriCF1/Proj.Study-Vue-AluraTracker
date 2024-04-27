5f121d92942b993d691c885b2510df99676fe0b8
Yuri Cruz <yuricruzf@gmail.com>
2024-04-26 23:39:06 -0300

    Createing folder to form of project

---
+++ src/Views/Projetos/Formulario.vue
@@ -0,0 +1,53 @@
+<template lang="">
+    <section class="projetos">
+        <h1 class="title">
+            Formulário
+        </h1>
+        <form @submit.prevent="salvarProjeto">
+            <div class="field">
+                <label class="label" for="nomeDoProjeto" >
+                    Nome do projeto
+                </label>
+                <input class="input" type="text" name="" id="nomeDoProjeto" v-model="nomeDoProjeto"/>
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+            </form>
+    </section>
+</template>
+<script>
+import { defineComponent, PropType, computed } from 'vue';
+
+// import IProjeto from '../Interfaces/IProjeto.ts'
+import { useStore } from '@/store';
+
+export default defineComponent({
+    name: 'FormularioComponent',
+    data() {
+        return {
+            nomeDoProjeto: '',
+        }
+    },
+    methods: {
+        salvarProjeto() {
+            this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto)
+            this.nomeDoProjeto = '';
+            this.$router.push('/projetos')
+        }
+    },
+    setup() {
+        const store = useStore()
+        return {
+            store,
+        }
+    }
+})
+</script>
+<style scoped>
+.projetos {
+    padding: 1.25rem
+}
+</style>
