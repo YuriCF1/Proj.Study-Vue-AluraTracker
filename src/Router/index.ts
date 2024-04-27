22b77fe9d08bdc453d8af5f54e3c7b0ae4a1697e
Yuri Cruz <yuricruzf@gmail.com>
2024-04-26 23:39:18 -0300

    Creating new routes

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,7 +1,7 @@
 import Tarefas from "@/Views/Tarefas.vue";
 import Projetos from "@/Views/Projetos.vue"
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
-
+import Formulario from "@/Views/Projetos/Formulario.vue";
 const rotas: RouteRecordRaw[] = [
     {
         path: '/',
@@ -12,6 +12,16 @@ const rotas: RouteRecordRaw[] = [
         path: '/projetos',
         name: 'Projetos',
         component: Projetos
+    },
+    {
+        path: '/projetos/novo',
+        name: 'Novo projeto',
+        component: Formulario
+    },
+    {
+        path: '/projetos/:id',
+        name: 'Editar Projeto',
+        component: Formulario
     }
 ];
 
