fa3304ae46e644005112e972261e77f9e6f813b9
Yuri Cruz <yuricruzf@gmail.com>
2024-04-30 00:14:39 -0300

    Adding new mutation

--- src/Views/Projetos/Formulario.vue
+++ src/Views/Projetos/Formulario.vue
@@ -1,64 +1,74 @@
 <template lang="">
-    <section class="projetos">
-        <h1 class="title">
-            Formulário
-        </h1>
-        <form @submit.prevent="salvarProjeto">
-            <div class="field">
-                <label class="label" for="nomeDoProjeto" >
-                    Nome do projeto
-                </label>
-                <input class="input" type="text" name="" id="nomeDoProjeto" v-model="nomeDoProjeto"/>
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-            </form>
-    </section>
+  <section class="projetos">
+    <h1 class="title">Formulário</h1>
+    <form @submit.prevent="salvarProjeto">
+      <div class="field">
+        <label class="label" for="nomeDoProjeto"> Nome do projeto </label>
+        <input
+          class="input"
+          type="text"
+          name=""
+          id="nomeDoProjeto"
+          v-model="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
 </template>
 <script>
-import { defineComponent, PropType, computed } from 'vue';
+import { defineComponent, PropType, computed } from "vue";
 
 // import IProjeto from '../Interfaces/IProjeto.ts'
-import { useStoreFunction } from '@/store';
+import { useStoreFunction } from "@/store";
 
 export default defineComponent({
-    name: 'FormularioComponent',
-    prop: {
-        idProps: {
-            type: String
-        }
-    },mounted() {
-        if(this.idProps) {
-            const buscandoProjeto = 
-            this.store.state.projetos.find(proj => proj.id == this.idProps)
-            this.nomeDoProjeto = buscandoProjeto?.nome
-        }
+  name: "FormularioComponent",
+  props: {
+    idProps: {
+      type: String,
     },
-    data() {
-        return {
-            nomeDoProjeto: '',
-        }
-    },
-    methods: {
-        salvarProjeto() {
-            this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto)
-            this.nomeDoProjeto = '';
-            this.$router.push('/projetos')
-        }
-    },
-    setup() {
-        const store = useStoreFunction()
-        return {
-            store,
-        }
+  },
+  mounted() {
+    console.log("Mounted IF");
+    if (this.idProps) {
+      const buscandoProjeto = this.store.state.projetos.find(
+        (proj) => proj.id == this.idProps
+      );
+      this.nomeDoProjeto = buscandoProjeto?.nome;
     }
-})
+  },
+  data() {
+    return {
+      nomeDoProjeto: "",
+    };
+  },
+  methods: {
+    salvarProjeto() {
+      if (this.idProps) {
+        this.store.commit("EDITA_PROJETO", {
+          id: this.idProps,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+        this.nomeDoProjeto = "";
+      }
+      this.$router.push("/projetos");
+    },
+  },
+  setup() {
+    const store = useStoreFunction();
+    return {
+      store,
+    };
+  },
+});
 </script>
 <style scoped>
 .projetos {
-    padding: 1.25rem
+  padding: 1.25rem;
 }
 </style>
