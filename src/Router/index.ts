58b9f4c118159b113db2560c07b8eb3c66abaed3
Yuri Cruz <yuricruzf@gmail.com>
2024-04-30 04:45:02 -0300

    Creating children paths

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,5 +1,8 @@
 import Tarefas from "@/Views/Tarefas.vue";
 import Projetos from "@/Views/Projetos.vue"
+
+import Lista from "../Views/Projetos/Lista.vue"
+
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import Formulario from "@/Views/Projetos/Formulario.vue";
 const rotas: RouteRecordRaw[] = [
@@ -10,19 +13,25 @@ const rotas: RouteRecordRaw[] = [
     },
     {
         path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
-    },
-    {
-        path: '/projetos/novo',
-        name: 'Novo projeto',
-        component: Formulario
-    },
-    {
-        path: '/projetos/:idProps',
-        name: 'Editar Projeto',
-        component: Formulario,
-        props:true //O roteador vai pegar o idProps e vai injetar na vizualisação como props 
+        component: Projetos,
+        children: [
+            {
+                path: '',
+                name: 'Lista',
+                component: Lista
+            },
+            {
+                path: 'novo',
+                name: 'Novo projeto',
+                component: Formulario
+            },
+            {
+                path: ':idProps',
+                name: 'Editar Projeto',
+                component: Formulario,
+                props: true //O roteador vai pegar o idProps e vai injetar na vizualisação como props 
+            }
+        ]
     }
 ];
 
