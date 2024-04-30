91534c10d535a5d26ae54de1a462468e887bf0f3
Yuri Cruz <yuricruzf@gmail.com>
2024-04-30 20:47:06 -0300

    Cleaning component to add new view and children links

--- src/Views/Projetos.vue
+++ src/Views/Projetos.vue
@@ -1,54 +1,17 @@
-<template lang="">
-  <section class="projetos">
-    <h1 class="titles">Projetos</h1>
-    <router-link to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </router-link>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(projeto, index) in listaProjetos" :key="index">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+<template>
+  <div class="projetosClass">
+    <h1 class="title">Projetos</h1>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-import { defineComponent, PropType, computed } from "vue";
-
-import IProjeto from "../Interfaces/IProjeto.ts";
-import { useStoreFunction } from "@/store";
-
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "ProjetosComponent",
-  setup() {
-    const store = useStoreFunction();
-    return {
-      listaProjetos: computed(() => store.state.projetos),
-    };
-  },
 });
 </script>
 <style scoped>
-.projetos {
-  padding: 1.25rem;
-}
+  .projetosClass {
+    padding: 1.25rem;
+  }
 </style>
