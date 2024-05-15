1bccbe4ede1a0ce7a000d7e4bf66b5e5562d6100
Yuri Cruz <yuricruzf@gmail.com>
2024-05-15 02:00:29 -0300

    Adding icon

--- src/Views/Projetos/Lista.vue
+++ src/Views/Projetos/Lista.vue
@@ -24,25 +24,40 @@
                 <i class="fas fa-pencil-alt"></i>
               </span>
             </router-link>
+            <button 
+            @click="excluirTarefa(projeto.id)"
+            class="button ml-2 is-danger">
+              <span class="icon is-small">
+                <i class="fas fa-trash"></i>
+              </span>
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
   </section>
 </template>
-<script>
+<script lang="ts">
 //   import { defineComponent, PropType, computed } from "vue";
 import { defineComponent, computed } from "vue";
 
 //   import IProjeto from "../Interfaces/IProjeto.ts";
 import { useStoreFunction } from "@/store";
 
+import { EXCLUIR_PROJETO } from "../../store/TipoDeMutacoes";
+
 export default defineComponent({
   name: "ListaComponent",
+  methods: {
+    excluirTarefa(idExclusao: string) {
+      this.store.commit(EXCLUIR_PROJETO, idExclusao)
+    }
+  },
   setup() {
     const store = useStoreFunction();
     return {
       listaProjetos: computed(() => store.state.projetos),
+      store
     };
   },
 });
