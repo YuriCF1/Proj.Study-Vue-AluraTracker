009dc4f352e79c741f980501d183830257b737db
Yuri Cruz <yuricruzf@gmail.com>
2024-05-15 03:40:24 -0300

    Getting models from store and implementing

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="notificacoes">
-    <article class="message is-success">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto legal</div>
-    </article>
-    <article class="message is-warning">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto legal</div>
-    </article>
-    <article class="message is-danger">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto legal</div>
+    <article
+      class="message is-success"
+      :class="contexto[note.tipo]"
+      v-for="(note, index) in notificacoes"
+      :key="index"
+    >
+      <div class="message-header">{{ note.tipo }}</div>
+      <div class="message-body">{{ note.text }}</div>
     </article>
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import { useStoreFunction } from "@/store";
+import { TipoDeNotificacao } from "@/Interfaces/INotificacao";
 export default defineComponent({
   name: "NotificacoesComponent",
+  data() {
+    return {
+      contexto: {
+        [TipoDeNotificacao.SUCCES]: "is-success",
+        [TipoDeNotificacao.WARNING]: "is-warning",
+        [TipoDeNotificacao.FAIL]: "is-danger",
+      },
+    };
+  },
+  setup(props) {
+    const store = useStoreFunction();
+    return {
+      notificacoes: computed(() => {
+        return store.state.notificacoes;
+      }),
+    };
+  },
 });
 </script>
 <style scoped>
